104d9ed73757747fc0576d4df9810d2efd36a244
unknown <you@example.com>
2025-09-13 02:36:44 +0700

    SanPham

--- WebsiteBanHang/obj/Debug/net8.0/WebsiteBanHang.AssemblyInfo.cs
+++ WebsiteBanHang/obj/Debug/net8.0/WebsiteBanHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsiteBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd472b53eee879f6e8e8ff515149f1c3552ba22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace181bba11d76c20fd63de7cc72e9de725f7d00")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsiteBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsiteBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
